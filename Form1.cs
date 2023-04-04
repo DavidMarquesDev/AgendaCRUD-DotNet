02ff6f1288fa13e48413dee079e905bfefaa87b5
441122229944 <davidtherion412294@gmail.com>
2023-04-04 14:39:16 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact List 1.0\nWritten bt : David", "About");
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'contactDBDataSet.People'. Você pode movê-la ou removê-la conforme necessário.
+            this.peopleTableAdapter.Fill(this.contactDBDataSet.People);
+
+        }
+    }
+}
